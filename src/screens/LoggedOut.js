24980f67d7ee0a6d7f71853b2f080b10fb5275bd
kashishbehl0310 <kashishbehl36@gmail.com>
2019-01-23 13:15:02 +0530

    Fixxing problems

--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -2,15 +2,40 @@ import React, { Component } from "react";
 import {  
     Text,
     View,
-    StyleSheet
+    StyleSheet,
+    Image
 } from "react-native";
+import colors from '../styles/colors'
 
 export default class LoggedOut extends Component{
     render(){
         return(
-            <Text>
-                Hello World
-            </Text>
+            <View style={styles.wrapper}>
+                <View>
+                    <Image 
+                        source={require('../img/airbnb-logo.png')} 
+                        style={styles.logo}
+                    />
+                </View>
+            </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        display: 'flex',
+        backgroundColor: colors.green01
+    },
+    welcomWrapper: {
+        flex: 1,
+        display: 'flex'
+    },
+    logo: {
+        width: 50,
+        height: 50,
+        marginTop: 50,
+        marginBottom: 40
+    }
+})
